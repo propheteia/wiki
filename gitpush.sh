59f8d04e916a79681be0f84284839c30ddcbc49f
wenbin <wenbin@ubuntu-wenbin.(none)>
2013-09-12 14:45:23 +0800

    auto

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git add .
 git commit -a -m "auto"
 git remote add origin git@github.com:propheteia/wiki.git
 git pull origin gh-pages
-git push origin gh-pages
+#git push origin gh-pages
 echo "****************************************************"
 echo "Succeed!"
 #notify-send   -t 2000 -i gtk-dialog-info "<span color=\"green\">git@gibhub has been synchronized</span>"
